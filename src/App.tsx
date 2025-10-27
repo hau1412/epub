4d17eb0b5fa874006c1c1a23bdd7e9694c861676
hau1412 <trananhhauvn@gmail.com>
2025-10-27 11:54:07 +0700

    Refactor App structure and update dependencies
    - Moved App component to src/App.tsx for better organization.
    - Simplified AppContent component by removing unnecessary imports and logic.
    - Updated Podfile.lock to reflect new dependency versions.
    - Adjusted index.js to import App from the new location.
    - Added a new line to README.md for clarity.

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@
  * @format
  */
 
+import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -20,7 +24,16 @@ function App() {
 }
 
 function AppContent() {
-  return <View style={styles.container} />;
+  const safeAreaInsets = useSafeAreaInsets();
+
+  return (
+    <View style={styles.container}>
+      <NewAppScreen
+        templateFileName="App.tsx"
+        safeAreaInsets={safeAreaInsets}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
